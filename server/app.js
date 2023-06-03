fa3fea2df28731e27259ad5fd38b6c29a28da931
shannonjohn <shanjohncantz@example.com>
2023-06-03 23:35:02 +0800

    add db connection

--- server/app.js
+++ server/app.js
@@ -1,9 +1,34 @@
 const express = require('express');
+const mysql = require('mysql2');
 const app = express();
 const port = 3001; 
+
+
+app.use(express.json());
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+app.get('/get',(req,res)=>{
+
+    res.send("Hello world");
+} )
+
+const connection = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: 'root2468',
+    database: 'hoteldb',
+  });
+
+  connection.connect((err) => {
+    if (err) {
+      console.error('Error connecting to MySQL:', err);
+      return;
+    }
+  
+    console.log('Connected to MySQL database');
+  });
 
 
