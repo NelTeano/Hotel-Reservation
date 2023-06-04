34004cc6516647da5018adc78bc04f897e86fc74
shannonjohn <shanjohncantz@example.com>
2023-06-04 22:37:50 +0800

    add post request for booking

--- server/routes/booking.js
+++ server/routes/booking.js
@@ -5,11 +5,11 @@ const pool = require('../database');
 // localhost:PORT/test
 booking.get('/test', (req, res) => {
   pool.execute('INSERT INTO `hotel_bookings` (`guest_name`, `check_in_date`,`check_out_date`, `room_type`,`num_guests` ) VALUES (?, ?,?,?,?)', [
-    'Juan',
-    '2023-09-10',
-    '2023-09-13',
+    'Maria',
+    '2023-09-16',
+    '2023-09-19',
     'deluxe',
-    '4'
+    '2'
   ], (err, result) => {
     if (err) {
       console.log('booking query error', err);
@@ -20,6 +20,8 @@ booking.get('/test', (req, res) => {
   });
 });
 
+
+//localhost:PORT/get/<id>
 booking.get('/get/:id', (req,res) =>{
   const book_id = req.params.id;
   pool.execute('SELECT * FROM `hotel_bookings` WHERE id=?',[book_id], (err, result) =>{
@@ -34,6 +36,29 @@ booking.get('/get/:id', (req,res) =>{
 
 
 })
+
+//localhost:PORT/api/book/
+booking.post( 'api/book/', (req,res) => {
+  const {guestName,checkinDate,checkoutDate,roomType,numGuest} = req.body;
+  pool.execute('INSERT INTO `hotel_bookings` (`guest_name`, `check_in_date`,`check_out_date`, `room_type`,`num_guests` ) VALUES (?, ?,?,?,?)', [
+    guestName,
+    checkinDate,
+    checkoutDate,
+    roomType,
+    numGuest
+  ], (err, result) => {
+    if (err) {
+      console.log('booking query error', err);
+    } else {
+      console.log('affected rows = ', result.affectedRows);
+      res.send('post success');
+    }
+  });
+
+}
+
+)
+
 
 
 module.exports = booking;
