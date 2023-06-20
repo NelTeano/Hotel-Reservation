3aac2820d3430d59602f054d333e89996328ec09
mrdcvlsc <mrdcvlsc@gmail.com>
2023-06-20 19:17:49 +0800

    apply database changes to the booking route

--- server/routes/booking.js
+++ server/routes/booking.js
@@ -2,7 +2,8 @@ const express = require('express');
 const booking = express.Router();
 const pool = require('../database');
 
-function yyyymmdd(date) {
+/** format date into yyyy-mm-dd string form. */ 
+function formatDate(date) {
   const yyyy = date.getFullYear().toString().padStart(4, '0');
   const mm = (date.getMonth() + 1).toString().padStart(2, '0');
   const dd = date.getDate().toString().padStart(2, '0');
@@ -15,23 +16,36 @@ function yyyymmdd(date) {
 booking.post('/submit', (req, res) => {
   console.log('req.body = ', req.body);
 
-  pool.execute(
-    'INSERT INTO `hotel_bookings` (`guest_name`, `check_in_date`, `check_out_date`, `room_type`, `num_guests`) VALUES (?, ?, ?, (SELECT id FROM room_types WHERE id=?), ?)', [
-      `${req.body.lastName}, ${req.body.firstName}`,
-      yyyymmdd(new Date(req.body.arriveDate)),
-      yyyymmdd(new Date(req.body.departDate)),
-      req.body.selectedRoomID,
-      req.body.guests
+  pool.execute(`
+    INSERT INTO hotel_bookings (
+      guest_name, check_in_date, check_out_date, room_type,
+      num_guests, booking_date, status_id, amount_paid, email
+    )
+    VALUES (
+      ?, ?, ?, (SELECT id FROM room_types WHERE id=?),
+      ?, ?, (SELECT id FROM status WHERE id=1),
+      (SELECT price FROM room_types WHERE id=?), ?
+    )`, [
+      `${req.body.firstName} ${req.body.lastName}`, // guest_name
+      formatDate(new Date(req.body.arriveDate)),    // check_in_date
+      formatDate(new Date(req.body.departDate)),    // check_out_date
+      req.body.selectedRoomID,                      // room_type
+      req.body.guests,                              // num_guests
+      formatDate(new Date()),                       // booking_date
+      req.body.selectedRoomID,                      // amount_paid - based on room_type price.
+      req.body.email                                // email
     ], (err, result) => {
       if (err) {
-        console.error('SQL ERROR : ', err);
+        res.status(500).json({ msg: 'failed to retrieve data'});
       } else {
-        console.debug('success - result : ', result);
+        if (result.affectedRows === 1) {
+          res.status(200).json({ msg: 'successfully booked'});
+        } else {
+          res.status(403).json({ msg: 'booking attempt failed'});
+        }
       }
     }
   );
-
-  res.status(200).json({ msg: 'form data received by the backend'});
 });
 
 module.exports = booking;
